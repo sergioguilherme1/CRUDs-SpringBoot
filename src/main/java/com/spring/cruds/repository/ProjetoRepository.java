a34b72549a5ab901378e96370429d0db18072b18
rodrigudp <rodrigo21437@gmail.com>
2026-03-24 09:39:40 -0300

    ProjetoService

--- src/main/java/com/spring/cruds/repository/ProjetoRepository.java
+++ src/main/java/com/spring/cruds/repository/ProjetoRepository.java
@@ -1,4 +1,9 @@
 package com.spring.cruds.repository;
 
-public interface ProjetoRepository {
+import com.spring.cruds.entity.Projeto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
 }
